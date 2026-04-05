2d7abe998edd663cf833df84236c6584506c3f9e
getwes <wesleyfreire1707@gmail.com>
2026-04-05 17:31:54 -0300

    parte de decisão

--- quiz.py
+++ quiz.py
@@ -4,3 +4,12 @@ if answer_user != "s":
     quit()
 
 print("começando...")
+
+print("quem desenvolveu o jogo grand theft auto (GTA)? \n (A) rockstar games \n (B) ubisoft \n (C) activision \n (D) EA \n")
+answer_1 = input("resposta:")
+
+if answer_1 == "A":
+    print("correto!")
+
+else:
+    print("incorreto")
