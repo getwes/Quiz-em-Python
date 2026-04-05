294cae1cc82b8b3d1704501d1719c780d323dc16
getwes <wesleyfreire1707@gmail.com>
2026-04-05 20:06:09 -0300

    parte de PONTUAÇÃO

--- quiz.py
+++ quiz.py
@@ -3,6 +3,8 @@ answer_user = input("quer começar? (s/n)")
 if answer_user != "s":
     quit()
 
+score = 0
+
 print("começando...")
 
 print("quem desenvolveu o jogo grand theft auto (GTA)? \n (A) rockstar games \n (B) ubisoft \n (C) activision \n (D) EA \n")
@@ -10,6 +12,19 @@ answer_1 = input("resposta:")
 
 if answer_1 == "A":
     print("correto!")
+    score = score + 1
 
 else:
     print("incorreto")
+
+print("qual o nome do protagonista do jogo Gts  san andreas ? \n (A) carlos john \n (B) carl jonhson \n (C) carl jaqueline \n (D) carlos jonhson \n")
+answer_2 = input("resposta:")
+
+if answer_2 == "B":
+    print("correto!")
+    score = score + 1
+
+else:
+    print("incorreto")
+
+print(f"quiz acabou.... pontuação: {score}/2")
