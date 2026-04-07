c2d39302e2794d3f87b85355c97e348ebf02e33a
getwes <wesleyfreire1707@gmail.com>
2026-04-07 10:37:03 -0300

    loop

--- guess_number.py
+++ guess_number.py
@@ -4,3 +4,12 @@ print("sejam muito bem vindo ao guess number")
 choice_number = input("digite o numero teto do desafio:")
 
 if choice_number.isdigit():
+   choicer_number = int(choice_number)
+else:
+    print("erro: valor informado não é numérico. favor  execute novamente e informe um numero!")
+    quit()
+
+random_number_number = random.radint(0, choice_number)
+
+while true:
+    answer_user = input("adivinhe o numero: ")
